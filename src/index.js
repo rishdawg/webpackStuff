0eb986969e55d1df9da7097c4a9f5219ba6bbbbf
Rishi Patel <rishirocks421@gmail.com>
2018-08-09 22:09:42 -0400

    got react loaded

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import _ from 'lodash';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
