3fd7a15523fdbfdf8364988d2709838ac883e7a6
rishdawg <rishirocks421@gmail.com>
2018-09-02 03:43:04 -0400

    got react loaded (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
 const path = require('path');
 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
   entry: './src/index.js',
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
